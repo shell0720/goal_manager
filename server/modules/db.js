41c2a1b0e159045b72a9418302789f35f7d77f2a
Michelle Wang <shell0720@gmail.com>
2017-04-24 16:44:01 -0500

    heroku created

--- server/modules/db.js
+++ server/modules/db.js
@@ -1,6 +1,12 @@
 var mongoose = require("mongoose");
 
-var mongoURI = "mongodb://localhost/goal_manager";
+// var mongoURI = "mongodb://localhost/goal_manager";
+
+ var mongoURI =
+'mongodb://mongodb://localhost/goal_manager'|
+'mongodb://michelle:12345@ds117821.mlab.com:17821/heroku_n6rx4jq0';
+
+
 var MongoDB = mongoose.connect(mongoURI).connection;
 
 MongoDB.on("error", function(err){
