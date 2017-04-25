2d56e2544cc989e823156835e6fbec8afa552bd5
Michelle Wang <shell0720@gmail.com>
2017-04-24 19:23:00 -0500

    update

--- server/modules/db.js
+++ server/modules/db.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 // var mongoURI = "mongodb://localhost/goal_manager";
 
  var mongoURI =
-'mongodb://mongodb://localhost/goal_manager'||
+// 'mongodb://mongodb://localhost/goal_manager'||
 'mongodb://heroku_n6rx4jq0:pa7ukf02gnejtf29akus43sb43@ds117821.mlab.com:17821/heroku_n6rx4jq0';
 
 mongoose.connect(mongoURI);
