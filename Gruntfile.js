1f601864b082af54a3f40e16e03ad00724981df5
Michelle Wang <shell0720@gmail.com>
2017-04-25 14:38:26 -0500

    gruntfile added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,111 @@
-module.exports = function(grunt) {
-    grunt.initConfig({
-       pkg: grunt.file.readJSON('package.json'),
-       uglify: {
-         options: {
-            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-         },
-         build: {
-            src: 'client/app.js',
-            dest: 'server/public/assets/scripts/app.min.js'
-         }
-       },
-       copy: {
-         main : {
-           expand: true,
-           cwd: "node_modules",
-           src: [
-              "angular/angular.min.js",
-              "angular/angular.min.js.map",
-              "angular/angular-csp.css"
-           ],
-           dest: "server/public/vendors/"
-         }
-       }
-    });
+module.exports = function(grunt){
+  grunt.initConfig({
+    pkg:grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
 
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+        expand: true,
+        cwd: 'client/scripts/',
+        src: '*.js',
+        dest: 'server/public/assets/scripts/',
+        ext: '.min.js'
+      }
+    },
 
-    grunt.registerTask('default', ['copy', 'uglify']);
-};
+    cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'client/styles',
+                    src: '*.css',
+                    dest: 'server/public/assets/styles/',
+                    ext: '.min.css'
+                }]
+            }
+    },
+
+
+    jshint:{
+      files:['client/scripts/*.js', 'server/app.js', 'server/modules/*.js', 'server/models/*.js']
+    },
+    watch: {
+      client:{
+        files: ['client/scripts/**/*.js',
+                'client/views/**/*.html',
+                'client/styles/*.css'
+               ],
+
+        tasks: ['jshint','cssmin', 'copy','uglify'],
+        options:{
+          spawn: false
+        }
+      },
+      server:{
+        files:['server/app.js', 'server/modules/*.js', 'server/models/*.js'],
+        tasks: ['jshint'],
+        options:{
+          spawn: false
+        }
+      }
+    },
+    copy: {
+      jQuery: {
+        expand: true,
+        cwd: "node_modules/jquery/dist/",
+        src: [
+          "jquery.min.js",
+        ],
+        dest: "server/public/vendors/"
+      },
+      html: {
+                expand: true,
+                cwd: 'client/views/',
+                src: [
+                    "index.html",
+                    "routes/*.html",
+                    "templates/*.html"
+                ],
+                "dest": "server/public/assets/views/"
+            }
+        },
+      jQuery : {
+        expand: true,
+        cwd: 'node_modules',
+        src: [
+           "jquery/*/*/*/*"
+         ],
+         "dest": "server/public/vendors/"
+      },
+      bootstrap : {
+        expand: true,
+        cwd: 'node_modules',
+        src: [
+           "bootstrap/*/*/*"
+         ],
+         "dest": "server/public/vendors/"
+      },
+
+      angular: {
+              expand: true,
+              cwd: 'node_modules',
+              src: [
+                  "angular/*",
+                  "angular-route/*"
+              ],
+              "dest": "server/public/vendors/"
+          },
+
+
+  });
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('default', ['jshint','copy','cssmin','uglify']);
+
+}
